76b7ad75720ecbc5fc7174a94d416982765b9547
Pooya Parsa Dadashi <pooya_parsa_dadashi@yahoo.com>
2022-02-18 08:38:23 +0330

    Create ci_multi_captcha_helper.php
    CodeIgniter Multi Captcha Helper file

---
+++ CIMultiCaptcha/Helpers/ci_multi_captcha_helper.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * --------------------------------------------------------------------------
+ * CodeIgniter Multi Captcha Helper file 
+ * --------------------------------------------------------------------------
+ *
+ * @package	        CodeIgniter-Multi-Captcha(CIMC)
+ * @collection	    Best Datamweb Tools CI4(BDT-CI4)
+ * @author	        Pooya Parsa Dadashi
+ * @link	        https://datamweb.ir
+ * @github_link	    https://github.com/datamweb/CodeIgniter-Multi-Captcha
+ * @since	        Version 1.0.0
+ * @datepublic	    2022-01-14 | 1400/10/24
+ * 
+ */
+  
