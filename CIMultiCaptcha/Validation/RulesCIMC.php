6fee84e273a91eb17fe7cb8102d0088b99114456
Pooya Parsa Dadashi <pooya_parsa_dadashi@yahoo.com>
2022-02-18 08:27:10 +0330

    Create RulesCIMC.php
    CodeIgniter Multi Captcha Validation Rules

---
+++ CIMultiCaptcha/Validation/RulesCIMC.php
@@ -0,0 +1,22 @@
+<?php
+namespace CIMC\Validation;
+
+/**
+ * --------------------------------------------------------------------------
+ * CodeIgniter Multi Captcha Validation Rules (CustomRules)
+ * --------------------------------------------------------------------------
+ *
+ * @package	        CodeIgniter-Multi-Captcha(CIMC)
+ * @collection	    Best Datamweb Tools CI4(BDT-CI4)
+ * @author	        Pooya Parsa Dadashi
+ * @link	        https://datamweb.ir
+ * @github_link	    https://github.com/datamweb/CodeIgniter-Multi-Captcha
+ * @since	        Version 1.0.0
+ * @datepublic	    2022-01-14 | 1400/10/24
+ * 
+ */
+
+ class RulesCIMC 
+ {
+     
+ }
