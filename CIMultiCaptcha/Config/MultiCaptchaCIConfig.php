4a3e9a9f3a1174aa3d458ca19ed38183617b005f
Pooya Parsa Dadashi <pooya_parsa_dadashi@yahoo.com>
2022-02-18 08:20:14 +0330

    Create MultiCaptchaCIConfig.php
    CodeIgniter Multi Captcha Configuration file.

---
+++ CIMultiCaptcha/Config/MultiCaptchaCIConfig.php
@@ -0,0 +1,22 @@
+<?php
+namespace CIMC\Config;
+
+use CodeIgniter\Config\BaseConfig;
+/**
+ * --------------------------------------------------------------------------
+ * CodeIgniter Multi Captcha Configuration
+ * --------------------------------------------------------------------------
+ *
+ * @package	        CodeIgniter-Multi-Captcha(CIMC)
+ * @collection	    Best Datamweb Tools CI4(BDT-CI4)
+ * @author	        Pooya Parsa Dadashi
+ * @link	        https://datamweb.ir
+ * @github_link	    https://github.com/datamweb/CodeIgniter-Multi-Captcha
+ * @since	        Version 1.0.0
+ * @datepublic	    2022-02-18 | 1400/11/29
+ * 
+ */
+class MultiCaptchaCIConfig extends BaseConfig
+{
+    
+}
