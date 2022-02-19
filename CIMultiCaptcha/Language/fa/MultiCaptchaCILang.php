e2f1d4f6195cbc29645561cd8217d565f54efadf
Pooya Parsa Dadashi <pooya_parsa_dadashi@yahoo.com>
2022-02-19 10:28:43 +0330

    Update MultiCaptchaCILang.php
    UPDATE

--- CIMultiCaptcha/Language/fa/MultiCaptchaCILang.php
+++ CIMultiCaptcha/Language/fa/MultiCaptchaCILang.php
@@ -4,13 +4,13 @@
  * CodeIgniter Multi Captcha language file (fa)
  * --------------------------------------------------------------------------
  *
- * @package	        CodeIgniter-Multi-Captcha(CIMC)
+ * @package         CodeIgniter-Multi-Captcha(CIMC)
  * @collection	    Best Datamweb Tools CI4(BDT-CI4)
- * @author	        Pooya Parsa Dadashi
- * @link	        https://datamweb.ir
- * @github_link	    https://github.com/datamweb/CodeIgniter-Multi-Captcha
- * @since	        Version 1.0.0
- * @datepublic	    2022-01-14 | 1400/10/24
+ * @author          Pooya Parsa Dadashi
+ * @link            https://datamweb.ir
+ * @github_link     https://github.com/datamweb/CodeIgniter-Multi-Captcha
+ * @since           Version 1.0.0
+ * @datepublic      2022-02-19 | 1400/11/30
  * 
  */
 
